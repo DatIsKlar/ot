e5884edd15f0b73ec1e809c62c3cf0cc762bc8bd
DatIsKlar <46826117+DatIsKlar@users.noreply.github.com>
2020-10-21 17:52:56 +0200

    Update maths.js

--- maths.js
+++ maths.js
@@ -2,78 +2,83 @@
 /* math.js */
 
 //subtracts two vectors a-b
-function VectorSubtract(a,b){
+function VectorSubtract(a,b)
+{
     x = a[0] - b[0];
     y = a[1] - b[1];
     z = a[2] - b[2];
-    var vec = [x,y,z];
-    return(vec)
+    return([x,y,z])
 }
 //addition of two vectors a+b
-function VectorAdd(a,b){ 
+function VectorAdd(a,b)
+{ 
     x = a[0] + b[0];
     y = a[1] + b[1];
     z = a[2] + b[2];
-    var vec = [x,y,z];
-    return(vec)
+    return([x,y,z])
 }
 //multiplication of two vectors a*b
-function VectorMultiply(a,b){
+function VectorMultiply(a,b)
+{
     x = a[0]*b[0];
     y = a[1]*b[1];
     z = a[2]*b[2];
-    var vec = [x,y,z];
-    return vec;
+    return ([x,y,z]);
 }
 
 //devides two vector a/b
-function VectorDivide(a,b){
+function VectorDivide(a,b)
+{
     x = a[0]/b[0];
     y = a[1]/b[1];
     z = a[2]/b[2];
-    var vec = [x,y,z];
-    return vec;
+    return ([x,y,z]);
 }
 
 //multiplication of a vector with an number(scalar)
-function VectorMultiplyScalar(a,b){
+function VectorMultiplyScalar(a,b)
+{
     x = a[0]*b;
     y = a[1]*b;
     z = a[2]*b;
-    var vec = [x,y,z];
-    return vec;
+    return ([x,y,z]);
 }
 
 //get the scalar(number) of two Vectors 
-function Dot(a,b){
+function Dot(a,b)
+{
    var vec = VectorMultiply(a,b);
    return vec[0]+vec[1]+vec[2];
 }
 
 //gets length of an forward vector
-function Length(forward){
+function Length(forward)
+{
     x = forward[0];
     y = forward[1];
     z = forward[2];
     return Math.sqrt( x * x + y * y + z * z );
 }
 //get the scalar(number) of an forward vector
-function Length2d_Sqr(forward){
+function Length2d_Sqr(forward)
+{
     return(forward[0]*forward[0]+forward[1]*forward[1]+forward[2]*forward[2]);
 }
 
 //distance between two points 
-function Distance(a,b){
-    var vec = VectorSubtract(a,b);
-    return Length(vec);
+function Distance(a,b)
+{
+    return Length(VectorSubtract(a,b));
 }
 
 //converts degrees to radians
-function DEG2RAD(deg){
+function DEG2RAD(deg)
+{
     return deg*(Math.PI/180);
 }
 //converts radians to degrees
-function RAD2DEG(rad){
+function RAD2DEG(rad)
+{
     return rad*(180/Math.PI);
 }
 
@@ -82,16 +87,16 @@ function Cross(a,b){
     x = a[1]*b[2] - a[2]*b[1];
 	y = a[2]*b[0] - a[0]*b[2];
 	z = a[0]*b[1] - a[1]*b[0];
+	return ([x,y,z]);
 }
 
 //get and forward vector from an angle like viewangles
 function AngleVectors(angle)
 {   
-	x = Math.cos(DEG2RAD(angle[0]))*Math.cos(DEG2RAD(angle[1]));
-	y = Math.cos(DEG2RAD(angle[0]))*Math.sin(DEG2RAD(angle[1]));;
+	x = Math.cos(DEG2RAD(angle[0]))*Math.cos(DEG2RAD(angle[2]));
+	y = Math.cos(DEG2RAD(angle[0]))*Math.sin(DEG2RAD(angle[2]));;
 	z = -Math.sin(DEG2RAD(angle[0]));
-    var forward = [x,y,z];
-    return forward;
+    return ([x,y,z]);
 }
 //normalized and then returns the viewangle
 function NormalizeAngles(angle)
@@ -113,8 +118,7 @@ function NormalizeAngles(angle)
 //calculates the fov between two angles
 function GetFov(viewangle, aimangle)
 {
-	var delta = VectorSubtract(viewangle,aimangle);
-	delta = NormalizeAngles(delta);
+	var delta = NormalizeAngles(VectorSubtract(viewangle,aimangle));
 	return Math.sqrt(Math.pow(delta[0], 2.00) + Math.pow(delta[1], 2.00));
 }
 //straight pasted from source sdk https://github.com/ValveSoftware/source-sdk-2013/blob/master/sp/src/mathlib/mathlib_base.cpp#L535
@@ -142,13 +146,7 @@ function VectorAngles(forward)
 		if (pitch < 0)
 			pitch += 360;
 	}
-	
-	x = pitch;
-	y = yaw;
-	z = 0;
-    var angles = [x,y,z];
-    
-	return angles;
+	return ([pitch,yaw,0]);
 }
 
 exports.VectorSubtract = VectorSubtract;
